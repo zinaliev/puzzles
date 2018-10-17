6b3552a72848b3726306fa01bf895cfb867db247
Roman Zinaliev <zinaliev@gmail.com>
2018-10-17 20:15:02 +0300

    remove dupes from sorted linked list (fb codelab)

---
+++ src/main/java/org/zinaliev/puzzles/list/RemoveDupesFromSortedList.java
@@ -0,0 +1,46 @@
+package org.zinaliev.puzzles.list;
+
+/**
+ * Given a sorted linked list, delete all duplicates such that each element appear only once.
+ *
+ * For example,
+ * Given 1->1->2, return 1->2.
+ * Given 1->1->2->3->3, return 1->2->3.
+ *
+ * Source - CodeLab (FB)
+ */
+public class RemoveDupesFromSortedList {
+
+  public ListNode deleteDuplicates(ListNode a) {
+    if (a == null || a.next == null)
+      return a;
+
+    ListNode curNode = a;
+    ListNode nextNode;
+
+    while (curNode.next != null) {
+
+      nextNode = curNode.next;
+
+      if (curNode.val == nextNode.val) {
+        curNode.next = nextNode.next;
+      } else {
+        curNode = nextNode;
+      }
+    }
+
+    return a;
+  }
+
+  // Definition for singly-linked list.
+  public static class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode(int x) {
+      val = x;
+      next = null;
+    }
+  }
+
+}
---
+++ src/test/java/org/zinaliev/puzzles/list/RemoveDupesFromSortedListTest.java
@@ -0,0 +1,72 @@
+package org.zinaliev.puzzles.list;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RemoveDupesFromSortedListTest {
+
+  private final RemoveDupesFromSortedList solution = new RemoveDupesFromSortedList();
+
+  @Test
+  public void test112233() {
+
+    Assert.assertArrayEquals(new int[] {1, 2, 3},
+        toArray(solution.deleteDuplicates(createList(1, 1, 2, 2, 3, 3)))
+    );
+  }
+
+  @Test
+  public void test55555() {
+
+    Assert.assertArrayEquals(new int[] {5},
+        toArray(solution.deleteDuplicates(createList(5, 5, 5, 5, 5)))
+    );
+  }
+
+  @Test
+  public void test1() {
+
+    Assert.assertArrayEquals(new int[] {1},
+        toArray(solution.deleteDuplicates(createList(1)))
+    );
+  }
+
+  private RemoveDupesFromSortedList.ListNode createList(int... items) {
+    RemoveDupesFromSortedList.ListNode head = new RemoveDupesFromSortedList.ListNode(items[0]);
+
+    RemoveDupesFromSortedList.ListNode curNode = head;
+
+    for (int i = 0; i < items.length - 1; i++) {
+      curNode.val = items[i];
+      curNode.next = new RemoveDupesFromSortedList.ListNode(items[i + 1]);
+
+      curNode = curNode.next;
+    }
+
+    return head;
+  }
+
+  private int[] toArray(RemoveDupesFromSortedList.ListNode head) {
+    int length = 0;
+
+    RemoveDupesFromSortedList.ListNode node = head;
+    while (node != null) {
+      length++;
+
+      node = node.next;
+    }
+
+    int index = 0;
+    int[] result = new int[length];
+
+    node = head;
+    while (node != null) {
+      result[index] = node.val;
+
+      node = node.next;
+      index++;
+    }
+
+    return result;
+  }
+}
