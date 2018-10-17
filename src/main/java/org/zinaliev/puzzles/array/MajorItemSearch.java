f54a57c501f8553fa69d80f2cdf531ac1ef72d2f
Roman Zinaliev <zinaliev@gmail.com>
2018-10-17 23:13:16 +0300

    majority element search in an array (fb codelab)

---
+++ src/main/java/org/zinaliev/puzzles/array/MajorItemSearch.java
@@ -0,0 +1,33 @@
+package org.zinaliev.puzzles.array;
+
+/**
+ * Given an array of size n, find the majority element. The majority element is the element that appears more than floor(n/2) times.
+ * <p>
+ * You may assume that the array is non-empty and the majority element always exist in the array.
+ * <p>
+ * Example :
+ * <p>
+ * Input : [2, 1, 2]
+ * Return  : 2 which occurs 2 times which is greater than 3/2.
+ */
+public class MajorItemSearch {
+
+  public int majorityElement(int[] a) {
+
+    int result = 0;
+    int counter = 0;
+
+    for (int i : a) {
+      if (counter == 0) {
+        result = i;
+        counter++;
+      } else if (i == result) {
+        counter++;
+      } else {
+        counter--;
+      }
+    }
+
+    return result;
+  }
+}
---
+++ src/test/java/org/zinaliev/puzzles/array/MajorItemSearchTest.java
@@ -0,0 +1,21 @@
+package org.zinaliev.puzzles.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MajorItemSearchTest {
+
+  private final MajorItemSearch solution = new MajorItemSearch();
+
+
+  @Test
+  public void test_1212121() {
+    assertEquals(1, solution.majorityElement(new int[]{1, 2, 1, 2, 1}));
+  }
+
+  @Test
+  public void test_1112222() {
+    assertEquals(2, solution.majorityElement(new int[]{1, 1, 1, 2, 2, 2, 2}));
+  }
+}
