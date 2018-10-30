e657a111a15b60a574c9cf4caf7a438e9e5d56ab
Roman Zinaliev <zinaliev@gmail.com>
2018-10-30 20:28:30 +0300

    remove n-th tailed item from LL (leetcode)

--- src/main/java/org/zinaliev/puzzles/list/RemoveNthFromEndNode.java
+++ src/main/java/org/zinaliev/puzzles/list/RemoveNthFromEndNode.java
@@ -2,55 +2,50 @@ package org.zinaliev.puzzles.list;
 
 /**
  * Given a linked list, remove the nth node from the end of list and return its head.
- *
+ * <p>
  * For example,
  * Given linked list: 1->2->3->4->5, and n = 2.
  * After removing the second node from the end, the linked list becomes 1->2->3->5.
- *
+ * <p>
  * Note:
  * * If n is greater than the size of the list, remove the first node of the list.
- *
+ * <p>
  * Try doing it using constant additional space.
  */
 public class RemoveNthFromEndNode {
 
-  public ListNode removeNthFromEnd(ListNode a, int b) {
-    int len = 0;
-    ListNode cur = a;
+  public static ListNode removeNthFromEnd(ListNode head, int n) {
+    if (head == null)
+      return null;
 
-    while (cur != null) {
-      len++;
-      cur = cur.next;
+    ListNode start = new ListNode(0);
+    start.next = head;
+
+    ListNode slow = start;
+    ListNode fast = start;
+
+    while (--n >= 0) {
+      if (fast.next == null)
+        break;
+
+      fast = fast.next;
     }
 
-    int k = b >= len ? 0 : len - b;
-
-    if (k == 0)
-      return a.next;
-
-    ListNode prev = a;
-    cur = prev.next;
-
-    int i = 1;
-    while (i < k) {
-
-      prev = cur;
-      cur = cur.next;
-      i++;
+    while (fast.next != null) {
+      slow = slow.next;
+      fast = fast.next;
     }
 
-    prev.next = cur.next;
+    if (slow.next != null)
+      slow.next = slow.next.next;
 
-    return a;
+    return start.next;
   }
 
-  class ListNode {
-    public int val;
-    public ListNode next;
-
-    ListNode(int x) {
-      val = x;
-      next = null;
-    }
-  }
+  /*
+  a) 1, n = 0
+  b) 1, n = 1
+  c) 1 -> 2 -> 3, n = 3
+  d) 1 -> 2 -> 3, n = 4
+   */
 }
